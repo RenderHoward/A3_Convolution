9fea9e64dac01db1478727defacf747755393eb9
RenderHoward <David.Howard@Ultrapoise.com>
2024-12-02 00:52:04 -0500

    Added Convolution step

--- Convo.m
+++ Convo.m
@@ -5,12 +5,12 @@ x = linspace( -3.7200, 3.7200, 187);
 MMM = [round((mmm(:,1)+3.72)/0.04) + 1 ,round(mmm(:,2)*3000) + 1, mmm(:,3) ];
 
 for i = 1:rows(MMM)
-      z(MMM(i,2),MMM(i,1)) = MMM(i,3);
+      z0(MMM(i,2),MMM(i,1)) = MMM(i,3);
 endfor
 
 [X,Y] = meshgrid(x,y);
 figure
-mesh(X,Y,z);
+mesh(X,Y,z0);
 title('convolve raw 0')
 
 mmm = dlmread ('convolve_raw_1.dat');
@@ -19,10 +19,12 @@ x = linspace( -8.16, 8.16, 409);
 MMM = [round((mmm(:,1)+8.16)/0.04) + 1 ,round(mmm(:,2)*3000) + 1, mmm(:,3) ];
 
 for i = 1:rows(MMM)
-      z(MMM(i,2),MMM(i,1)) = MMM(i,3);
+      z1(MMM(i,2),MMM(i,1)) = MMM(i,3);
 endfor
 
 [X,Y] = meshgrid(x,y);
 figure
-mesh(X,Y,z);
+mesh(X,Y,z1);
 title('convolve raw 1')
+
+conv = conv2( z0, z1)
